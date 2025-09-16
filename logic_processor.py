4c9a9f33df1a331e39e21f33e777c0aa13ee0b43
Cong <50347840+nvcong89@users.noreply.github.com>
2025-09-16 14:28:49 +0700

    first change
    first change

--- logic_processor.py
+++ logic_processor.py
@@ -12,12 +12,16 @@ ACTIVE_BOT: list[Agent] = [
 
 def CalculateStrategy(processed_data):
     close_price = processed_data[-1][3]
+    longPrice = close_price - 1.0
+    shortPrice = 99999
 
     for agent in ACTIVE_BOT:
         result = agent.Calculate(processed_data)
         if result == True: # MUST CHECK FOR BOOL, SINCE AGENT MAY RETURN NONE TOO!
             ENTRADE_CLIENT.Order("VN30F2509", "NB", None, None, 1, "MTL", True)
             print(f"{agent.name} đã đặt lệnh LONG tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
+
         elif result == False:
             ENTRADE_CLIENT.Order("VN30F2509", "NS", None, None, 1, "MTL", True)
-            print(f"{agent.name} đã đặt lệnh SHORT tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
+            print(f"{agent.name} đã đặt lệnh SHORT tại giá {(close_price):.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
+
